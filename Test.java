8ca3e220af0bc863aabd787125939a241c8f7031
302934218 <302934218@qq.com>
2018-05-28 10:11:47 +0800

    第一次提交

---
+++ Test.java
@@ -0,0 +1,14 @@
+//张三写的代码
+class Test{
+	public static void main(String[], args){
+		
+	}
+	
+	public void zhangsan(){
+		system.out.println("张三");
+	}
+	
+	public void lisi(){
+		system.out.println("李四");
+	}
+}
